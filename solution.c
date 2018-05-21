a03de26a34567aceb03db51d1118dedaab6fdd88
piotr <piotr_p@windowslive.com>
2018-05-21 11:05:52 +0200

    quick fix

--- solution.c
+++ solution.c
@@ -47,29 +47,30 @@ int main(int argc, char* argv[])
                     outText[i++]=text[position];
                     outText[i++]=text[++position];
                 }
-            for(j=0; j<tmp; j++)
+            for(j=0; j<tmp && i<textLength; j++)
                 {
                     outText[i++]=text[--position];
                 }
             tmp -= secondRound;
-            for(j=0; j<tmp; j++)
+            for(j=0; j<tmp && i<textLength; j++)
                 {
                     position-=dimension;
                     outText[i++]=text[position];
                 }
-            for(j=0; j<tmp; j++)
+            for(j=0; j<tmp && i<textLength; j++)
                 {
                     outText[i++]=text[++position];
 
                 }
             tmp--;
-            for(j=0; j<tmp; j++)
+            for(j=0; j<tmp && i<textLength; j++)
                 {
                     position+=dimension;
                     outText[i++]=text[position];
                 }
             secondRound = 1;
         }
-    printf("%s",outText);
+    //printf("%s",outText);
+    printf("%.*s", textLength, outText);
     return 0;
 }
